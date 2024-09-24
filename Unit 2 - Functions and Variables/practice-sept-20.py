92be8bd72574830a77575bdbb4b3d80219f026b5
jacobgallagher <820046@mystma.org>
2024-09-24 15:19:37 -0500

    9/24

---
+++ Unit 2 - Functions and Variables/practice-sept-20.py
@@ -0,0 +1,6 @@
+fav_animal = str(input("name of your favorite animal: "))
+why = str(input("why? "))
+weight = str(input("how much does it weight?"))
+location = str(input("where does it live? "))
+
+print("Your favorite animal is a " + fav_animal + " Beacuse " + why + ". It weighs " + weight + ", and it lives in " + location + ".")
---
+++ Unit 2 - Functions and Variables/practice-sept-24.py
@@ -0,0 +1,6 @@
+Favorite_animal = input("What is your favorite animal?\n>")
+print("Your favorite animal is " + Favorite_animal)
+
+num = input("Enter a number?\n>")
+num = int(num)
+print(num * 10)
