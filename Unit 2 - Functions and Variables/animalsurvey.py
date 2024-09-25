6369b57cebb2ec39fb2611a86f5322a507190327
jacobgallagher <820046@mystma.org>
2024-09-25 15:03:07 -0500

    animalsurvey

---
+++ Unit 2 - Functions and Variables/animalsurvey.py
@@ -0,0 +1,6 @@
+fav_animal = str(input("name of your favorite animal: ")) # ask what the user's favorite animal is
+why = str(input("why? ")) # ask the user why?
+weight = str(input("how much does it weight?")) # ask how mmuch it weighs
+location = str(input("where does it live? ")) # ask the user where the animal lives
+
+print("Your favorite animal is a " + fav_animal + " Beacuse " + why + ". It weighs " + weight + ", and it lives in " + location + ".") #give the user a summary about the information of their favorite animal 
