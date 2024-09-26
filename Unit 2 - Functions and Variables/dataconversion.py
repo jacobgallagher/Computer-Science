a167b0166f6367299a6610246a1ff28970f90337
jacobgallagher <820046@mystma.org>
2024-09-26 14:36:47 -0500

    data conversion september 26

---
+++ Unit 2 - Functions and Variables/dataconversion.py
@@ -0,0 +1,33 @@
+a = 115         #int -> string
+a_str = str(a)
+print(a_str)
+b = 3.14        #float -> string
+float_str = str(b)
+print(float_str)
+c = "68"        #string -> int
+num = float(c)
+print(num)
+d = "True"      #string -> boolean
+bool_val = bool(d)
+print(bool_val)
+e = True        #boolean -> string
+bool_str = str(e)
+print(bool_str)
+f = False       #boolean -> string
+bool_strr = str(f)
+print(bool_strr)
+g = '10110111'  #byte -> int
+num = int(g, 2)
+print(num)
+h = "2.54"      #string -> float
+sfloat = float(h)
+print(sfloat)
+i = 100         #int -> float
+num_float = float(i)
+print(num_float)
+j = 10.0        #float -> int
+num2 = int(j)
+print(num2)
+k = 254         #int -> byte
+binary_num = bin(k)
+print(binary_num)
