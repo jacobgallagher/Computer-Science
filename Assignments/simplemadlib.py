365a0ca3491eceaf27ff463a3fc4e090975222d4
jacobgallagher <820046@mystma.org>
2024-09-26 15:05:12 -0500

    simple mad lib

---
+++ Assignments/simplemadlib.py
@@ -0,0 +1,15 @@
+# Once upon a time in a large [resturant].
+# there lived a humongus [bug].
+# Everyday the cockroach would [verb] vigorously.
+# One day, a(n) [adjective] appeared.
+# And [verb] the [bug].
+# the cockroach was [emotion].
+
+restaurant = input("Please enter a restaurant.\n>")
+bug = input("Please enter a bug.\n>")
+verb = input("Please enter a verb.\n>")
+adjective = input("Please enter an adjective.\n>")
+verb2 = input("Please enter the past tense variation another verb.\n>")
+emotion = input("Please enter an emotion.\n>")
+
+print("once upon a time in a large " + restaurant + ", there lived a humungous " + bug + ". Everyday the " + bug + " would " + verb + " vigorously. One day, a(n) " + adjective + " health inspector appeared, and " + verb2 + " the " + bug + ". The " + bug + " was " + emotion + ".")
