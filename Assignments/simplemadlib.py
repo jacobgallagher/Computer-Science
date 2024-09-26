a030740b2b8d818a3850223052ed450be38ded01
jacobgallagher <820046@mystma.org>
2024-09-26 15:19:21 -0500

    simple mad lib version 2

--- Assignments/simplemadlib.py
+++ Assignments/simplemadlib.py
@@ -4,12 +4,16 @@
 # One day, a(n) [adjective] appeared.
 # And [verb] the [bug].
 # the cockroach was [emotion].
-
 restaurant = input("Please enter a restaurant.\n>")
 bug = input("Please enter a bug.\n>")
 verb = input("Please enter a verb.\n>")
 adjective = input("Please enter an adjective.\n>")
 verb2 = input("Please enter the past tense variation another verb.\n>")
 emotion = input("Please enter an emotion.\n>")
-
 print("once upon a time in a large " + restaurant + ", there lived a humungous " + bug + ". Everyday the " + bug + " would " + verb + " vigorously. One day, a(n) " + adjective + " health inspector appeared, and " + verb2 + " the " + bug + ". The " + bug + " was " + emotion + ".")
+noun = input("Please enter a noun.\n>")
+verb3 = input("Please enter a verb in the past tense.\n>")
+food = input("please enter a food item.\n>")
+utensil = input("Please enter an eating utensil.\n>")
+time = input("Please enter a time.\n>")
+print("After the health inspector had his run in with the " + bug + ", he went home. As he entered into his home, he tripped on a " + noun + " before his wife and kids " + verb3 + " at him. Then he goes to the dinner table where " + food + " was waiting for him. He ate it quickly with a " + utensil + ". Finally, he goes to sleep at " + time + ".")
