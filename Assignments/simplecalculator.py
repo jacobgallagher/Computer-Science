114f3b49a2ecc3b74209da64f3828aa9c4c68445
jacobgallagher <820046@mystma.org>
2024-09-30 15:03:08 -0500

    simplecalculator sept 30

---
+++ Assignments/simplecalculator.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    print(x + y)
+add(5, 10)
+
+def add(x, y):
+    print(x - y)
+add(5, 10)
+
+def add(x, y):
+    print(x * y)
+add(5, 10)
+
+def add(x, y):
+    print(x / y)
+add(5, 10)
