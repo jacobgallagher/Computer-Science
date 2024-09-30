dc15a8340db9d0b3554a0b4695ec92a4d0f49876
jacobgallagher <820046@mystma.org>
2024-09-30 14:23:52 -0500

    top5movies sep 30

---
+++ Assignments/top5movies.py
@@ -0,0 +1,9 @@
+def my_movies(a, b, c, d, e):       # Define the function with a name
+    print("your top 5 movies!")
+    print("1. " + a)
+    print("2. " + b)
+    print("3. " + c)
+    print("4. " + d)
+    print("5. " + e)
+
+my_movies("skibidi shooter", "the matrix", "the conjuring", "the stories of jonkler in kurger bing", "the lego movie")
