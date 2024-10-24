d07d7741a6489b574dd78881d73397c4b4685cf8
jacobgallagher <820046@mystma.org>
2024-10-24 15:19:59 -0500

    adventurea game 10 24

--- Unit 3 - Conditionals and Exceptions/text_adventure_game.py
+++ Unit 3 - Conditionals and Exceptions/text_adventure_game.py
@@ -1,11 +1,13 @@
 import time
 import random
 
+
 score = 0
 opponent_score = 0
 
+
 def one_v_one():
-       
+    
     print("Matchmaking... (Ranked Duel - Diamond 1 Division 4)\n")
     time.sleep(2.5)
     print("Match Found\n")
@@ -25,21 +27,21 @@ def one_v_one():
     print("Kickoff in 2...\n")
     time.sleep(1)
     print("Kickoff in 1...\n")
+    kickoff()
 
 
-    first_kickoff_selection_1 = input("What kickoff are you gonna use?\nA) Speed Flip Kickoff\nB)Delayed Kickoff\nC)Fake Kickoff\n").lower()
-    if first_kickoff_selection_1 == "a":
-        first_one_v_one_speed_kickoff()
-    if first_kickoff_selection_1 == "b":
-        first_one_v_one_delayed_kickoff()
-    if first_kickoff_selection_1 == "c":
-        first_one_v_one_fake_kickoff()
+def kickoff():
+    kickoff_selection = input("What kickoff are you gonna use?\nA) Speed Flip Kickoff\nB)Delayed Kickoff\nC)Fake Kickoff\n").lower()
+    if kickoff_selection == "a":
+        kickoff_outcome_1()
+    if kickoff_selection == "b":
+        kickoff_outcome_2()
+    if kickoff_selection == "c":
+        kickoff_outcome_3()
+       
 
-def third_kickoff():
-    print("Game Point!")
-    win_chance = input("")
 
-def first_one_v_one_speed_kickoff():
+def kickoff_outcome_1():
     win_chance = random.random()
     if win_chance <= .4:
         print("You won the kickoff And scored, the score is now " + score + 1 + " - " + opponent_score + "\n")
@@ -49,21 +51,97 @@ def first_one_v_one_speed_kickoff():
         print("Kickoff in 2...\n")
         time.sleep(1)
         print("Kickoff in 1...\n")
-        win_first_speed_kickoff_1()
-    elif win_chance > .4:
-        print("You lost the kickoff and your opponent scored, the score is now 0-1!\n")
+        second_kickoff_1()
+    if win_chance  > .4 and win_chance <= .8:
+        print("You won the kickoff and the ball is on Megagamer768's side!")
+        choose_to_shoot()
+    elif win_chance > .8:
+        print("You lost the kickoff and your opponent scored, the score is now " + score + + " - " + opponent_score + 1 + "\n")
         time.sleep(3)
         print("Kickoff in 3...\n")
         time.sleep(1)
         print("Kickoff in 2...\n")
         time.sleep(1)
         print("Kickoff in 1...\n")
-        lost_first_speed_kickoff_1()
+        second_kickoff_1()
 
-def lost_first_speed_kickoff_1():
-    print("")
+def choose_to_shoot():
+    choice = input("Would you like to\nA) Shoot the ball\nB) Play defensive and go back to your goal post")
+    if choice == "a":
+        shooting_ball()
+    if choice == "b":
+        defend_2()
+    else:
+        print("Invalid input, try again.")
+        choose_to_shoot()
 
-def win_first_speed_kickoff_1():
+
+
+
+
+    if selection == "a":
+        win_chance = random.random()
+        if win_chance <= .8:
+            print("You got to the ball first!")
+            win_chance = random.random()
+            if win_chance <= .85:
+                print(username + "scored! The score is now " + score + 1 + " - " + opponent_score + "\n")
+                third_kickoff()
+            else:
+                print("You hit the ball off of the crossbar.")
+                crossbar_decision()
+               
+           
+        else:
+            print("Megagamer768 got to the ball before you, the ball is now on your side. Defend.")
+            defend()
+def go_to_goal():
+    win_chance = random.random()
+    if win_chance <= .5:
+        print("You made it back to the goal!")
+        defend()
+    else: 
+        print("You didnt make it back in time. The score is now " + score + 1 + " - " + opponent_score + "\n")
+
+def press_f():
+    F = input("Press F to go to try to get to your goal and defend!").lower()
+    if F == "f":
+            go_to_goal()
+    else:
+        print("Invalid input, try again.")
+        press_f()
+
+def rush_ball():
+    win_chance = random.random()
+    if win_chance <= .3:
+        print("You didn't make it to the ball fast enough\n")
+        press_f()
+def boost_over_ball():
+    win_chance = random.random()
+    if win_
+def select_after_kickoff():
+    selection = input("You won the kickoff, would you like to \nA) Rush the ball without boost. \nB) Go to boost instead of the ball. \nC) Try to demolish Megagamer768.\n").lower()
+    if selection == "a":
+        rush_ball()
+    if selection == "b":
+        boost_over_ball()
+    if selection == "c":
+        demolish()
+    else:
+        print("Invalid input, try again.")
+
+
+def kickoff_outcome_2():
+    win_chance = random.random()
+    if win_chance <= .7:
+        select_after_kickoff()
+    if win_chance > .7:
+        print("You lost the kickoff, the ball is going towards your goal.")
+        defend()
+        second_kickoff_1()
+
+
+def second_kickoff_1():
     second_kickoff_selection_2 = input("What kickoff are you gonna use?\nA) Speed Flip Kickoff\nB)Delayed Kickoff\nC)Fake Kickoff").lower()
     if second_kickoff_selection_2 == "a":
         time.sleep(1)
@@ -78,7 +156,8 @@ def win_first_speed_kickoff_1():
         print
     else:
         print("Invalid input, try again.")
-        win_first_speed_kickoff_1()
+        second_kickoff_1()
+
 
 def defend():
     button_press = input("Press X to defend.\n").lower()
@@ -89,12 +168,36 @@ def defend():
         else:
             print("You failed to defend. Megagamer768 scored on you and the score is now " + score + " - " + opponent_score + 1 + "\n")
 
+def defend_2():
+    button_press = input("Press X to defend.\n").lower()
+    if button_press == "x":
+        win_chance = random.random()
+        if win_chance <= .50:
+            print("Megagamer768 hit the ball towards you. You managed to defend!")
+            shooting_ball()
+        else:
+            print("You failed to defend. Megagamer768 scored on you and the score is now " + score + " - " + opponent_score + 1 + "\n")
+
 def shooting_ball():
     input("Press X to shoot the ball!\n")
-    print("You scored the ball! The score is now 1-1")
+    score_chance = random.random()
+    if score_chance <= .7:
+        print("You scored! The score is now " + score + 1 + " - " + opponent_score + "\n")
+    else:
+        print("You missed the shot.\n")
+        a_to_defend()
 
-                       
-def defending():
+
+
+def a_to_defend():
+    press = input("Press A to go back and defend!").lower()
+    if press == "a":
+        defend()
+    else:
+        print("Invalid input, try again.")
+        a_to_defend()
+
+def defending_shot():
     button_press = input("Press X to defend.\n").lower()
     if button_press == "x":
         win_chance = random.random()
@@ -104,6 +207,7 @@ def defending():
         else:
             print("You failed to defend. Megagamer768 scored on you and the score is now .")
 
+
 def double_touch():
     win_chance  = random.random()
     if win_chance <= .6:
@@ -112,10 +216,11 @@ def double_touch():
         x = input("You missed the double touch. Press X to go back to your goal and defend!").lower()
         if x == "x":
             defending()
-            
+           
         else:
             print("Invalid input, try again.")
             double_touch()
+
 
 def crossbar_decision():
     choice = input("Would you like to\nA) Jump for the double touch\nB) Back off and defend.").lower()
@@ -125,7 +230,9 @@ def crossbar_decision():
         defending()
         print("Megagamer768 scored on you and the score is now ")
 
-            
+
+           
+
 
 def offensive_kickoff():
     selection = input("Would you like to \nA) Rush the ball without boost. \nB) Go to boost instead of the ball. \nC) Try to demolish Megagamer768.\n").lower()
@@ -140,11 +247,15 @@ def offensive_kickoff():
             else:
                 print("You hit the ball off of the crossbar.")
                 crossbar_decision()
-                
-            
+               
+           
         else:
             print("Megagamer768 got to the ball before you, the ball is now on your side. Defend.")
             defend()
+
+
+
+
 
 
 
@@ -159,6 +270,8 @@ def first_one_v_one_delayed_kickoff():
         lost_first_speed_kickoff_1()
 
 
+
+
 def first_one_v_one_fake_kickoff():
     win_speed_kickoff_chance = random.random()
     if win_speed_kickoff_chance <= .8:
@@ -168,9 +281,11 @@ def first_one_v_one_fake_kickoff():
         print("You lost the kickoff and your opponent scored, the score is now 0-1.\n")
         lost_first_speed_kickoff_1()
 
+
 username = input("What is your username?\n>")
 time.sleep(1)
 input("Press any button to continue.\n")
+
 
 def game_welcome():
     print("Welcome To Rocket League!")
@@ -185,9 +300,13 @@ def game_welcome():
         print("Invalid choice, try again.")
         game_welcome()
 
+
 game_welcome()
 
+
 def won_first_speed_kickoff_1():
+
+
 
 
     print("Kickoff in 3\n")
@@ -200,12 +319,20 @@ def won_first_speed_kickoff_1():
         print("")
 
 
+
+
 def two_v_two():
     print("Matchmaking... (Ranked 2v2 - Champion 2 Division 3)")
+
+
 
 
 def three_v_three():
     print("Matchmaking...  (Ranked 3v3 - Platinum 1 Division 4)")
 
 
+
+
 #r = random.randrange(0,10) ozowski
+
+
